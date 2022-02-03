9549f7b5ef11dd543b2bed0bda08907f12ad758f
TsunoKosuke <ltlonhammer@gmail.com>
2022-02-04 05:20:08 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,11 @@
+function openLine(){
+    window.open("https://lin.ee/7b7Ip2r");
+}
+
+function openTwitter(){
+    window.open("https://twitter.com/kawabayu")
+}
+
+function openInstagram(){
+    window.open("https://www.instagram.com/kawabayu/")
+}
