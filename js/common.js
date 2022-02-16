705092a675a1c7523d8a49db18d7ebb49f55f22a
TsunoKosuke <ltlonhammer@gmail.com>
2022-02-16 14:21:50 +0900

    kawabayu_1

--- js/common.js
+++ js/common.js
@@ -9,3 +9,37 @@ function openTwitter(){
 function openInstagram(){
     window.open("https://www.instagram.com/kawabayu/")
 }
+
+function openBlog(){
+    window.open("http://kawabayu.asablo.jp/blog/")
+}
+
+$(function(){
+    // #で始まるアンカーをクリックした場合に処理
+    $('a[href^=#]').click(function() {
+       // スクロールの速度
+       var speed = 350; // ミリ秒
+       // アンカーの値取得
+       var href= $(this).attr("href");
+       // 移動先を取得
+       var target = $(href == "#" || href == "" ? 'html' : href);
+       // 移動先を数値で取得
+       var position = target.offset().top;
+       // スムーススクロール
+       $('body,html').animate({scrollTop:position}, speed, 'swing');
+       return false;
+    });
+ });
+
+// $(function () {
+//     $(window).scroll(function () {
+//         const wHeight = $(window).height();
+//         const scrollAmount = $(window).scrollTop();
+//         $('.scrollanime').each(function () {
+//             const targetPosition = $(this).offset().top;
+//             if(scrollAmount > targetPosition - wHeight + 60) {
+//                 $(this).addClass("fadeInDown");
+//             }
+//         });
+//     });
+// });
